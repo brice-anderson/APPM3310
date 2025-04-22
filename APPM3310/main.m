3428b54953650d23116b749aa1d694a4398c2502
brice-anderson <brice.anderson@colorado.edu>
2025-04-22 17:36:07 -0600

    Fixed eigenvector calculation, now outputs ranks back

--- APPM3310/main.m
+++ APPM3310/main.m
@@ -1,8 +1,10 @@
 clc; clear; close all;
 % Read file in
 matrix = readmatrix("SECMatrix.xlsx",'Range','B2:Q17');
+
 for i = 1:length(matrix)
-    matrix(i,i) = sum(matrix(:,i));
+    %matrix(i,i) = 0.5;
+    matrix(i,i) = sum(matrix(i,:))/18;
 end
 
 try chol(matrix)
@@ -11,4 +13,8 @@ catch ME
     disp('Matrix is not symmetric positive definite')
 end
 [U,S] = eig(matrix);
+team_ranks = (matrix*U(:,1))';
+writematrix(team_ranks,"SECMatrix.xlsx",'Range','B20:Q20')
+
+
 
